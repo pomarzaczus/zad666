9484ed8e0921ed922130170eb714bff2c42f186e
szymon.m.olejniczak <szymon.m.olejniczak@student.put.poznan.pl>
2025-04-10 23:41:22 +0200

    nie bede tego debugowac  nie ma opcji z 4 h juz debuguje

--- WinFormsApp2/Form3.Designer.cs
+++ WinFormsApp2/Form3.Designer.cs
@@ -32,12 +32,16 @@
             label1 = new Label();
             czas = new Label();
             timer1 = new System.Windows.Forms.Timer(components);
+            debug = new Label();
+            timer2 = new System.Windows.Forms.Timer(components);
+            label2 = new Label();
+            label3 = new Label();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(252, 422);
+            label1.Location = new Point(242, 458);
             label1.Name = "label1";
             label1.Size = new Size(29, 15);
             label1.TabIndex = 0;
@@ -46,7 +50,7 @@
             // czas
             // 
             czas.AutoSize = true;
-            czas.Location = new Point(328, 422);
+            czas.Location = new Point(300, 458);
             czas.Name = "czas";
             czas.Size = new Size(38, 15);
             czas.TabIndex = 1;
@@ -55,14 +59,57 @@
             // 
             // timer1
             // 
+            timer1.Enabled = true;
             timer1.Interval = 1000;
             timer1.Tick += timer1_Tick;
+            // 
+            // debug
+            // 
+            debug.AllowDrop = true;
+            debug.AutoEllipsis = true;
+            debug.AutoSize = true;
+            debug.CausesValidation = false;
+            debug.LiveSetting = System.Windows.Forms.Automation.AutomationLiveSetting.Assertive;
+            debug.Location = new Point(569, 371);
+            debug.Name = "debug";
+            debug.Size = new Size(38, 15);
+            debug.TabIndex = 2;
+            debug.Text = "label2";
+            // 
+            // timer2
+            // 
+            timer2.Enabled = true;
+            timer2.Interval = 1000;
+            timer2.Tick += timer2_Tick;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(585, 435);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 3;
+            label2.Text = "label2";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(593, 275);
+            label3.Name = "label3";
+            label3.Size = new Size(38, 15);
+            label3.TabIndex = 4;
+            label3.Text = "label3";
+            label3.Click += label3_Click;
             // 
             // Form3
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(798, 508);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(debug);
             Controls.Add(czas);
             Controls.Add(label1);
             Name = "Form3";
@@ -75,6 +122,10 @@
 
         private Label label1;
         private Label czas;
-        private System.Windows.Forms.Timer timer1;
+        public System.Windows.Forms.Timer timer1;
+        public System.Windows.Forms.Timer timer2;
+        public Label debug;
+        private Label label2;
+        private Label label3;
     }
 }
--- WinFormsApp2/Form3.cs
+++ WinFormsApp2/Form3.cs
@@ -7,6 +7,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
+using System.Timers;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+using System.Xml.Linq;
+using System.Web;
 
 namespace WinFormsApp2
 {
@@ -17,65 +22,349 @@ namespace WinFormsApp2
             InitializeComponent();
         }
         Form1 formularz;
-        int czas1;
+        int czas1 = 0;
+
+        int kroxy;
+        int dydalf;
+        int szop;
         public Form3(Form1 test)
         {
             formularz = test;
-            czas.Text = czas1.ToString();
-
+            czas1 = 0;
+            kroxy = formularz.kroxy;
+            dydalf = formularz.dyd;
+            szop = formularz.szopy;
             InitializeComponent();
+            create_plansza();
+
+
         }
         int polozenieX;
         int polozenieY;
 
-        class leButton:Button
+        public class leButton : Button
         {
-            public bool dydalf;
-            public bool krox;
-            public bool szop;
+            public bool dydalf = false;
+            public bool krox = false;
+            public bool szop = false;
+            public int i;
+            public int j;
         }
         List<leButton> butons = [];
         private void create_plansza()
         {
-            
-            
+            var rand = new Random();
+
             float someDouble = this.Size.Width;
-            polozenieX = (int)(0.1 * someDouble);
+            polozenieX = (int)(0.01 * someDouble);
             someDouble = this.Size.Height;
-            polozenieY = (int)(0.1 * someDouble);
-            int width =400;
+            polozenieY = (int)(0.01 * someDouble);
+            int width = 400;
             int height = 400;
             int licznik = 0;
-            for (int i = 0; i < formularz.x; i++) {
+            int[] tab = new int[formularz.x * formularz.y]; //losowanie wartosci
+            int ileadds = kroxy + dydalf + szop;
+            for (int i = 0; i < tab.Length; i++)
+            {
+                if (kroxy > 0)
+                {
+                    tab[i] = 1;
+                    kroxy--;
+                }
+                else
+                if (dydalf > 0)
+                {
+                    tab[i] = 2;
+                    dydalf--;
+                }
+                else
+                if (szop > 0)
+                {
+                    tab[i] = 3;
+                    szop--;
+                }
+                else
+                {
+                    tab[i] = 0;
+                }
+
+
+            }
+            Random rnd = new Random();
+            List<int> tab_szuhffle = [];
+            foreach (var item in tab)
+            {
+                int position = rnd.Next(tab_szuhffle.Count);
+                tab_szuhffle.Add((position == tab_szuhffle.Count) ? item : tab_szuhffle[position]);
+                tab_szuhffle[position] = item;
+            }
+
+            for (int i = 0; i < tab.Length; i++)
+            {
+                debug.Text += " " + tab_szuhffle[i];
+            }
+
+
+            for (int i = 0; i < formularz.x; i++)
+            {
                 for (int j = 0; j < formularz.y; j++)
                 {
-                    butons[licznik] = new leButton();
-                    butons[licznik].Height = width / formularz.y;
-                    butons[licznik].Width = height / formularz.x;
+                    butons.Add(new leButton());
+                    //butons[licznik].Height = width / formularz.y;
+                    //butons[licznik].Width = height / formularz.x;
+                    butons[licznik].Height = width / formularz.x;
+                    butons[licznik].Width = height / formularz.y;
                     butons[licznik].ForeColor = Color.Blue;
-                    butons[licznik].Location = new Point(polozenieY +j* butons[licznik].Height, polozenieX + j * butons[licznik].Height);
+                    butons[licznik].Location = new Point(polozenieY + j * butons[licznik].Width, polozenieX + i * butons[licznik].Height);
                     butons[licznik].Click += new EventHandler(klik);
-                    butons[licznik].Name = i.ToString()+"_"+j.ToString();
+                    butons[licznik].Name = i.ToString() + "_" + j.ToString();
+                    butons[licznik].i = i;
+                    butons[licznik].j = j;
+                    this.Controls.Add(butons[licznik]);
+                    butons[licznik].Visible = true;
+                    butons[licznik].Show();
+                    switch (tab_szuhffle[licznik])
+                    {
+                        case 1:
+
+                            butons[licznik].krox = true;
+                            //   butons[licznik].Text = "D";
+                            break;
+                        case 2:
+                            butons[licznik].dydalf = true;
+                            //  butons[licznik].Text = "K";
+                            break;
+                        case 3:
+                            butons[licznik].szop = true;
+                            //   butons[licznik].Text = "S";
+                            break;
+                        default:
+                            // butons[licznik].Text = "O";
+                            break;
+                    }
                     //losowanie kroxuw , szopy dydunie
                     licznik++;
                 }
-                
+
 
             }
+
+
             //this.Size.Width;
         }
+        int kroxy_licznik = 0;
+        int dydalf_licznik = 0;
+        int szop_licznik = 0;
+        private static System.Timers.Timer aTimer;
+        private static szopTimet aTimerSZOP;
+        class szopTimet : System.Timers.Timer
+        {
+            public Form3 fomular;
+            public leButton buton;
+        }
+        bool timerKROXActive = false;
         private void klik(object sender, EventArgs e)
         {
             leButton temp = (leButton)sender; //sender as leButton;
+            label3.Text = "i = " + temp.i.ToString() + "  j = " + temp.j.ToString();
+            if (czas.Text != "koniec" || czas.Text != "win")
+            {
+                if (timer1.Enabled == false) czas.Text = "koniec";
+                if (temp.dydalf)
+                {
+                    dydalf_licznik++;
+                    if (dydalf_licznik > formularz.dyd - 1)
+                    {
+                        timer1.Stop();
+                        czas.Text = "win";
+                        return;
+                    }
+                    debug.Text = "D";
+                    temp.Text = "D";
+                    return;
+                }
+                else
+                if (temp.krox)
+                {
+                    if (timerKROXActive)
+                    {
+                        duration = 0;
+                        timerKROXActive = false;
+                        aTimer.Stop();
+                        return;
+                    }
+                    temp.Text = "K";
+
+                    aTimer = new System.Timers.Timer();
+                    aTimer.Interval = 100;
+                    aTimer.Elapsed += OnTimedEvent;
+                    timerKROXActive = true;
+                    aTimer.Start();
+                    return;
+                }
+                else
+                if (temp.szop)
+                {
+                    temp.Text = "S";
+                    aTimerSZOP = new szopTimet();
+                    aTimerSZOP.Interval = 2000;
+                    aTimerSZOP.buton = temp;
+                    aTimerSZOP.Elapsed += OnTimedEvent2;
+                    aTimerSZOP.fomular = this;
+                    aTimerSZOP.Start();
+
+                    return;
+                }
+                else
+                {
+                    temp.Text = "O";
+                }
+
+            }
+            
+        }
+        int duration = 0;
+
+        private void popUp()
+        {
+            timer1.Stop();
+            timer1.Enabled = false;
+            //czas.Text = "koniec";
+            System.Windows.Forms.MessageBox.Show("BUllShit");
+            // this.Close();
+        }
+        int currentX;
+        int currentY;
+        string nameofcurrent;
+        bool szopelapesd = false;
+        private void OnTimedEvent2(Object source, System.Timers.ElapsedEventArgs e)
+        {
+
+            szopTimet temp = (szopTimet)source;
+            //temp.fomular.debug.Text = "XXXXX";
+            int tempX = 0;
+            int tempY = 0;
+            string jezyutempowbrakuje = "";
+            bool czyY = false;
+            for (int i = 0; i < temp.buton.Name.Length; i++)
+            {
+                if (temp.buton.Name[i] == '_')
+                {
+                    tempX = Int32.Parse(jezyutempowbrakuje);
+                    jezyutempowbrakuje = "";
+                    czyY = true;
+                    continue;
+                }
+                if (czyY)
+                {
+                    jezyutempowbrakuje += temp.buton.Name[i];
+
+                }
+                else
+                {
+                    jezyutempowbrakuje += temp.buton.Name[i];
+                }
+
+            }
+            tempY = Int32.Parse(jezyutempowbrakuje);
+            currentX = tempX;
+            currentY = tempY;
+            szopelapesd = true;
+            nameofcurrent = temp.buton.Name;
+            temp.Stop();
+        }
+
+
+
+        private void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
+        {
+            duration++;
+            if (duration > 10)
+            {
+                aTimer.Stop();
+                timer1.Stop();
+                popUp();
+
+
+
+            }
         }
         private void timer1_Tick(object sender, EventArgs e)
         {
-            czas1 -= 1;
+            czas1++;
+            timer1.ToString();
             czas.Text = czas1.ToString();
-            if (czas1 == 0)
+            // czas.Text = timer1.ToString();
+            if (czas1 > formularz.czas)
             {
-                czas.Text = " koniec ";
+                czas.Text = "koniec";
+                timer1.Stop();
+                popUp();
+
+                return;
             }
+            if (timer1.Enabled == false)
+            {
+                czas.Text = "koniec";
+            }
+        }
+
+        private void czas_Click(object sender, EventArgs e)
+        {
+
+        }
+        public void xdddddddddddddddddddddd(int tempX, int tempY)
+        {
+            int licznik = 0;
+            debug.Text = nameofcurrent + " ";
+            foreach (leButton item in butons)
+            {
+                if (item.Name == nameofcurrent)
+                {
+                    item.Text = "";
+                    if (item.i + 1 < formularz.x) { butons[licznik + 1].Text = ""; debug.Text += (licznik + formularz.x).ToString() + " "; } // i 3 licz 15            
+
+                    if (item.i - 1 >= 0) { butons[licznik - 1].Text = ""; debug.Text += (licznik - formularz.x).ToString() + " "; }// i 1 licz 5
+                    if (item.j + 1 < formularz.y && licznik + formularz.x < butons.Count) { butons[licznik + formularz.x].Text = ""; debug.Text += (licznik + 1).ToString() + " "; } // j 1 licz 11
+                    if (item.j - 1 >= 0 && licznik - formularz.x >= 0){ butons[licznik - formularz.x].Text = ""; debug.Text += (licznik - 1).ToString() + " "; } //j -1 licz null
+                    break; 
+                }
+                licznik++;
+            }
+
+            
+            
+            /* if (tempX + tempY * tempX + 1 < butons.Count) { butons[tempX + tempY * tempX + 1].Text = ""; }
+             if (tempX + tempY * tempX - 1 >= 0) { butons[tempX + tempY * tempX - 1].Text = ""; }
+             if (tempX + tempY * tempX - tempY >= 0) { butons[tempX + tempY * tempX - tempY].Text = ""; }
+             if (tempX + tempY * tempX + tempY < butons.Count) { butons[tempX + tempY * tempX + tempY].Text = ""; }*/
+        }
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            label2.Text = szopelapesd.ToString();
+            if (szopelapesd)
+            {
+                xdddddddddddddddddddddd(currentX, currentY);
+                szopelapesd = false;
+            }
+
+            if (czas.Text == "koniec")
+            {
+                for (int i = 0; i < butons.Count; i++)
+                {
+                    butons[i].Enabled = false;
+                }
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
